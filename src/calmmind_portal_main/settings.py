bb148ccf817683132f3492b28f18441b01fa0b9b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-15 16:09:51 +0530

    Changes

--- src/calmmind_portal_main/settings.py
+++ src/calmmind_portal_main/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Added STATICFILES_DIRS to include the static folder
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 

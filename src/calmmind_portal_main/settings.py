17773b5a0929cda4e4533b8e9df97eac87eb5999
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-15 15:25:40 +0530

    Changes

--- src/calmmind_portal_main/settings.py
+++ src/calmmind_portal_main/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'clammind',
-    'clammind.apps.ClammindConfig',
+    'calmmind.apps.CalmmindConfig',
 ]
 
 MIDDLEWARE = [
